cb7810fc56b57f995db5eb3501d1ce2267dbb839
michael0chen <github@michaelchen.net>
2024-03-26 17:19:05 -0400

    My changes after one round of the tutorial.
    - app/ui/dashboard/nav-links: Added select/hover borders to nav items.
    - package.json: Added port to dev and start commands.

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,37 +1,70 @@
+'use client';
+
 import {
-  UserGroupIcon,
-  HomeIcon,
-  DocumentDuplicateIcon,
+	UserGroupIcon,
+	HomeIcon,
+	DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+	{ name: 'Home', href: '/dashboard', icon: HomeIcon },
+	{
+		name: 'Invoices',
+		href: '/dashboard/invoices',
+		icon: DocumentDuplicateIcon,
+	},
+	{ name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
-  return (
-    <>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
-        return (
-          <a
-            key={link.name}
-            href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </a>
-        );
-      })}
-    </>
-  );
+	const pathname = usePathname();
+	return (
+		<>
+		{links.map((link) => {
+			const LinkIcon = link.icon;
+			return (
+				<Link
+					key={link.name}
+					href={link.href}
+					// CSS for nav inactive nav items
+					// https://tailwindcss.com/docs/border-color
+					//   - Added border and border hover color
+					className={clsx(
+						'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium border-2 hover:border-blue-600 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+						{
+							// CSS for selected nav item
+							//   - Stronger border color but less than hover
+							'border-blue-200 bg-sky-100 text-blue-600': pathname === link.href,
+						},
+					)}
+// Original code
+//					className={clsx(
+//						'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+//						{
+//							'bg-sky-100 text-blue-600': pathname === link.href,
+//						},
+//					)}
+				>
+					<LinkIcon className="w-6" />
+					<p className="hidden md:block">{link.name}</p>
+				</Link>
+			);
+//			return (
+//				<a
+//					key={link.name}
+//					href={link.href}
+//					className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+//				>
+//					<LinkIcon className="w-6" />
+//					<p className="hidden md:block">{link.name}</p>
+//				</a>
+//			);
+		})}
+		</>
+	);
 }
